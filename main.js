e49928371f1ac2df697957fe1ff7e5dcd1f8de51
IamAshley <korokozazh@gmail.com>
2021-09-07 14:36:08 +0300

    made functions for day time

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-var x = document.body.style;
-x.backgroundColor = "gray"
-function dark()
+// var x = document.body.style;
+// x.backgroundColor = "gray"
+function dark() {
+    document.body.style.backgroundColor = "black";
+}
+function light(){
+    document.body.style.backgroundColor = "white"
+}
+
+
 //test
