c96e3feaaf2eb96b0f99594550249648081c13e6
IamAshley <korokozazh@gmail.com>
2021-09-17 15:21:32 +0300

    made buttons +|-

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // var x = document.body.style;
 // x.backgroundColor = "gray"
+const mainHeading = document.getElementById("main");
 function dark() {
     document.body.style.backgroundColor = "black";
+    mainHeading.style.backgroundColor="purple";
 }
 function light(){
-    document.body.style.backgroundColor = "white"
+    document.body.style.backgroundColor = "white";
+    mainHeading.style.backgroundColor="pink";
+}
+var size = 65;
+
+function plus(){
+    size = size+10
+    mainHeading.style.fontSize = `${size}px`;
+
 }
 
 
-//test
+function minus(){
+    size = size-5
+    mainHeading.style.fontSize = `${size}px`;
+
+}
